730f212ecba30160361bfcceafcf1b61a747b602
MTen <MichaelTener@gmail.com>
2014-02-03 18:02:35 -0500

    most awesome test ever written

--- spec/controllers/countries_controller_spec.rb
+++ spec/controllers/countries_controller_spec.rb
@@ -2,21 +2,27 @@ require 'spec_helper'
 
 describe CountriesController do
   before do
-    @country = Country.create(name: "Worldwide", woeid: 1)
+    @country = Country.create(name: "country", woeid: 1)
+    @country2 = Country.create(name: "country2", woeid: 2)
   end
 
-  describe "when visiting index" do
+  describe 'when visiting index' do
     before do
       get :index
     end
 
-    it "should render the index page" do
+    it 'should render the index page' do
       expect(response).to render_template("index")
     end
 
-    it "retrieves all tweets" do
+    it 'retrieves all countries' do
       assigns(:countries).should == Country.all
     end
+
+    it 'should display two countries' do
+      expect(@country & @country2)
+    end
+
   end
 end
 

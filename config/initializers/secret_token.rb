743bae10e092a6fe727885b673150a1675b23ceb
Michael Muse <michaelmuse@gmail.com>
2014-02-03 15:31:34 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twendy::Application.config.secret_token = '1fec5fa5f1c217e4c812f917e9d1331fde74a5ad3a68cca153ecf3db6af4481b34b1ab8aba0b2907b924257809cdb528893f656149657bfee787fb6005218ae8'

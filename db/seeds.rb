a5d199f50bd850188195c5ac7f28b32fcb48d854
iwoodruff <isaac.woodruff@gmail.com>
2014-02-03 21:05:32 -0500

    commented out anachronistic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,9 @@
 # ================================
 # CREATES COUNTRIES AND ADDS WOEID
+#       anachronistic code
 # ================================
 
-
-
-
-
-all_countries = HTTParty.get("http://where.yahooapis.com/v1/countries?appid=Q6S.eLHV34GwNc79pswEdclgszSHyCyV7u5nb4kCEkfySEnahkUqyCEN1W1o2LsXR40GWpY")
-all_countries["places"]["place"].each do |hash|
-  Country.create(name: hash["name"], woeid: hash["woeid"])
-end
+# all_countries = HTTParty.get("http://where.yahooapis.com/v1/countries?appid=Q6S.eLHV34GwNc79pswEdclgszSHyCyV7u5nb4kCEkfySEnahkUqyCEN1W1o2LsXR40GWpY")
+# all_countries["places"]["place"].each do |hash|
+#   Country.create(name: hash["name"], woeid: hash["woeid"])
+# end

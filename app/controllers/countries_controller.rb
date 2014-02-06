b8eccdbc4c479793e039bd5ffb8706505448854e
Michael Muse <michaelmuse@gmail.com>
2014-02-06 12:55:37 -0500

    some changes to get json of past trends

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -9,11 +9,17 @@ class CountriesController < ApplicationController
     end
 	end
 
+###wants interval, trend_index, rank, trend_name
+
 	def show
 		country = Country.find_by_name(params[:name])
-		time = country.get_latest_trends_timing
+		time = country.get_trends_times(1)
 		@trends = country.get_cohort_of_trends(time)
-
+##EDITING 
+    @trends.each do |trend|
+      array_attrs = country.find_past_attrs_for_trend(trend[:trend_id])
+    end
+#########
 		respond_to do |format|
       format.html
       format.json { render :json => @trends.to_json }

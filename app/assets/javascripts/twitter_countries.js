f98f30a247ce5e98e2e203251ae427defebb419b
liormb <liormb@gmail.com>
2014-02-07 08:42:20 -0500

    fixed the trendsD3 table that wasn't showing up on country change

--- app/assets/javascripts/twitter_countries.js
+++ app/assets/javascripts/twitter_countries.js
@@ -47,8 +47,8 @@ TrendsListView.prototype = {
         $ul = $('.trend-container ul');
 
 		trendsD3([],trends);
-		$chart.empty();
-		$chart.fadeTo(1500, 1);
+		$chart.fadeTo(1000, 0);
+		$chart.fadeTo(1000, 1);
 		
 		var curr_trends = [];
 		$.each(trends, function(index, trend) {

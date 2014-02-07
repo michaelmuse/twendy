19dee8a1ff54ce42e8fd8b9803556a29cc5b1271
liormb <liormb@gmail.com>
2014-02-07 11:59:12 -0500

    sync

--- app/assets/javascripts/world_tour.js
+++ app/assets/javascripts/world_tour.js
@@ -127,15 +127,17 @@ function globus(error, world, names) {
 		    	// rotate automatically only if set to true (and pick the next country)
           // pry country.all to make twitter array
           //if country(all) is in twitcountries(twitter only), keep looping the line below, if i gets above countries.length, go back to i=0
-          var twitterCountriesArr = ["Argentina","Australia","Belgium","Brazil","Canada","Chile","Colombia","Dominican Republic","Ecuador","France","Germany","Greece","Guatemala","India","Indonesia","Ireland","Italy","Japan","Kenya","Korea","Malaysia","Mexico","Netherlands","New Zealand","Nigeria","Norway","Pakistan","Peru","Philippines","Poland","Portugal","Russia","Singapore","South Africa","Spain","Sweden","Turkey","Ukraine","United Arab Emirates","United Kingdom","United States","Venezuela"];
-          var j = i - 1;
-          do {
-            (j < countries.length) ? j++ : j=0;
-  		    	(auto_rotate) ? country = countries[j] : title.text(country.name);
-          } while (twitterCountriesArr.indexOf(country.name) == -1)
-          i = j;
+         //  var twitterCountriesArr = ["Argentina","Australia","Belgium","Brazil","Canada","Chile","Colombia","Dominican Republic","Ecuador","France","Germany","Greece","Guatemala","India","Indonesia","Ireland","Italy","Japan","Kenya","Korea","Malaysia","Mexico","Netherlands","New Zealand","Nigeria","Norway","Pakistan","Peru","Philippines","Poland","Portugal","Russia","Singapore","South Africa","Spain","Sweden","Turkey","Ukraine","United Arab Emirates","United Kingdom","United States","Venezuela"];
+         //  var j = i - 1;
+         //  do {
+         //    (j < countries.length) ? j++ : j=0;
+  		    	// (auto_rotate) ? country = countries[j] : title.text(country.name);
+         //  } while (twitterCountriesArr.indexOf(country.name) == -1)
+         //  i = j;
 
-          $('header h1').text(country.name);
+         //  $('header h1').text(country.name);
+
+          (auto_rotate) ? country = countries[j] : title.text(country.name);
 
 		      var p = d3.geo.centroid(country),
 		          r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
